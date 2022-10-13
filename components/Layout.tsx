84fcb2ed13379bda4836568ecd3fc2b4e0da5362
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-10-13 15:49:34 +0530

    collapsable siedbar on desktop screen

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { BlogPages } from '../src/utils/parse-properties'
 import SideBar from './Sidebar'
 import { AiOutlineDoubleRight } from 'react-icons/ai'
+import { useCycle } from 'framer-motion'
 function Layout({
   children,
   blogData,
@@ -12,33 +13,50 @@ function Layout({
 }) {
   const [isSidebarOpen, setSidebarOpen] = useState(false)
   const toggleSidebar = () => setSidebarOpen(!isSidebarOpen)
+  const [openSidebar, cycleOpenSidebar] = useCycle(true, false);
   return (
-    <div className="flex-container">
-      <div>
-        <SideBar
-          isSidebarOpen={isSidebarOpen}
-          toggleSidebar={toggleSidebar}
-          blogData={blogData}
-        />
+    <div>
+      {!openSidebar && (
+        <Show above="md">
+          <IconButton
+            variant={"outline"}
+            mx="1"
+            onClick={() => cycleOpenSidebar()}
+            aria-label="open close drawer"
+            icon={<AiOutlineDoubleRight />}
+          />
+        </Show>
+      )}
 
-        <Box flex="1" px="1">
-          <Flex py="4" alignItems="center">
-            <Show below="md">
-              <IconButton
-                variant={'outline'}
-                mx="1"
-                onClick={() => toggleSidebar()}
-                aria-label="open close drawer"
-                icon={<AiOutlineDoubleRight />}
-              />
-            </Show>
-          </Flex>
-        </Box>
+      <div className="flex-container">
+        <div>
+          <SideBar
+            isSidebarOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
+            blogData={blogData}
+            cycleOpenSidebar={cycleOpenSidebar}
+            openSidebar={openSidebar}
+          />
+
+          <Box flex="1" px="1">
+            <Flex py="4" alignItems="center">
+              <Show below="md">
+                <IconButton
+                  variant={"outline"}
+                  mx="1"
+                  onClick={() => toggleSidebar()}
+                  aria-label="open close drawer"
+                  icon={<AiOutlineDoubleRight />}
+                />
+              </Show>
+            </Flex>
+          </Box>
+        </div>
+
+        <div className="flex-item">{children}</div>
       </div>
-
-      <div className="flex-item">{children}</div>
     </div>
-  )
+  );
 }
 
 export default Layout
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -5,132 +5,194 @@ import {
   Show,
   Text,
   useBreakpointValue,
-} from '@chakra-ui/react'
-import Link from 'next/link'
-import React from 'react'
-import { BlogPages } from '../../src/utils/parse-properties'
+} from "@chakra-ui/react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AnimatePresence, motion, useCycle } from "framer-motion";
+import { BlogPages } from "../../src/utils/parse-properties";
 
 import {
+  Button,
   Drawer,
   DrawerOverlay,
   DrawerCloseButton,
+  DrawerHeader,
   DrawerBody,
   DrawerContent,
   VStack,
-} from '@chakra-ui/react'
-import { AiOutlineDoubleLeft } from 'react-icons/ai'
+  Collapse,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { AiOutlineDoubleLeft } from "react-icons/ai";
 
 interface Props {
-  onClose: Function
-  isOpen: boolean
-  variant: string
-  blogData: BlogPages[]
+  onClose: Function;
+  isOpen: boolean;
+  variant: string;
+  blogData: BlogPages[];
+  openSidebar: boolean;
+  cycleOpenSidebar: ()=>void;
 }
-const smVariant = { navigation: 'drawer', navigationButton: true }
-const mdVariant = { navigation: 'sidebar', navigationButton: false }
-
-const SidebarContent = ({ blogData }: { blogData: BlogPages[] }) => {
+const smVariant = { navigation: "drawer", navigationButton: true };
+const mdVariant = { navigation: "sidebar", navigationButton: false };
+interface SidebarContentProps { 
+  blogData: BlogPages[];
+  isSidebarOpen: boolean;
+  toggleSidebar: ()=>void;
+}
+const SidebarContent = ({
+  blogData,
+  isSidebarOpen,
+  toggleSidebar,
+}: SidebarContentProps) => {
   return (
     <VStack w="100%">
-      <Flex py="4" w="100%" justifyContent={'space-between'} alignItems="left">
-        <Link href="/">
-          <Text
-            ml={4}
-            p={1}
-            fontSize={{ base: 'sm', md: 'md', lg: 'lg' }}
-            fontWeight={900}
-            _hover={{
-              bg: '#E2E2E1',
-              cursor: 'pointer',
-              borderRadius: 'md',
-            }}
-          >
-            Flaq Academy
-          </Text>
-        </Link>
-        {/* <Show above="md">
+      <Flex
+        py="4"
+        w="100%"
+        justifyContent={"space-between"}
+        alignItems="center"
+      >
+        <Link href="/">Flaq Academy</Link>
+        <Show above="md">
           <IconButton
-            variant={'outline'}
+            variant={"outline"}
             mx="1"
+            onClick={() => toggleSidebar()}
             aria-label="open close drawer"
             icon={<AiOutlineDoubleLeft />}
           />
-        </Show> */}
+        </Show>
       </Flex>
       {blogData?.map((blog) => (
         <Flex
-          fontSize={{ base: 'sm', md: 'md', lg: 'lg' }}
+          fontSize="14px"
           color="#37352F"
           key={blog.pageId}
-          // fontFamily="Segoe UI"
-          fontWeight={'700'}
+          fontFamily="Segoe UI"
+          fontWeight={"600"}
           px="2"
-          borderRadius={'2'}
+          borderRadius={"2"}
           py="1"
           _hover={{
-            bg: '#E2E2E1',
-            cursor: 'pointer',
-            borderRadius: 'md',
+            bg: "#E2E2E1",
           }}
         >
-          {blog.icon ?? <Text px="1">{blog.icon}</Text>}
-          <Link href={`/blog/${blog.pageId}`}>
-            <Text>{blog.title}</Text>
-          </Link>
+          <Text px="1">{blog.icon}</Text>
+          <Link href={`/blog/${blog.pageId}`}>{blog.title}</Link>
         </Flex>
       ))}
     </VStack>
-  )
-}
-
-const SidebarDrawar = ({ isOpen, variant, onClose, blogData }: Props) => {
-  return variant === 'sidebar' ? (
-    <Box
-      px="4"
-      bgColor="#FBFBFA"
-      borderRight={'2px solid #ebebe1'}
-      mx="1"
-      left={0}
-      w="298px"
-      top={0}
-      minH="100%"
-    >
-      <SidebarContent blogData={blogData} />
-    </Box>
+  );
+};
+const sideVariants = {
+  closed: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: 1,
+    },
+  },
+};
+const SidebarDrawar = ({
+  isOpen,
+  variant,
+  onClose,
+  blogData,
+  openSidebar,
+  cycleOpenSidebar,
+}: Props) => {
+  return variant === "sidebar" ? (
+    <AnimatePresence>
+      {openSidebar && (
+        <motion.aside
+          initial={{ width: 0 }}
+          animate={{
+            width: 300,
+          }}
+          exit={{
+            width: 0,
+            transition: { delay: 0.7, duration: 0.3 },
+          }}
+        >
+          <motion.div
+            className="container"
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={sideVariants}
+          >
+            <Box
+              px="4"
+              bgColor="#FBFBFA"
+              borderRight={"2px solid #ebebe1"}
+              mx="1"
+              left={0}
+              w="298px"
+              top={0}
+              minH="100%"
+            >
+              <SidebarContent
+                blogData={blogData}
+                isSidebarOpen={openSidebar}
+                toggleSidebar={cycleOpenSidebar}
+              />
+            </Box>
+          </motion.div>
+        </motion.aside>
+      )}
+    </AnimatePresence>
   ) : (
     <Drawer
-      size={'full'}
+      size={"full"}
       isOpen={isOpen}
       placement="left"
       onClose={() => onClose()}
     >
       <DrawerOverlay>
-        <DrawerContent minH={'100%'}>
+        <DrawerContent minH={"100%"}>
           <DrawerCloseButton />
-          <DrawerBody minW={'85%'} w="85%" minH={'100%'} p="0">
-            <SidebarContent blogData={blogData} />
+          <DrawerHeader>Flaq Academy</DrawerHeader>
+          <DrawerBody minW={"85%"} w="85%" minH={"100%"} p="0">
+            <SidebarContent
+              isSidebarOpen={openSidebar}
+              toggleSidebar={cycleOpenSidebar}
+              blogData={blogData}
+            />
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
     </Drawer>
-  )
-}
+  );
+};
+// TODO: Create a new responsive sidebar component.
 interface SidebarProps {
-  isSidebarOpen: boolean
-  blogData: BlogPages[]
-  toggleSidebar: () => void
+  isSidebarOpen: boolean;
+  blogData: BlogPages[];
+  toggleSidebar: () => void;
+  cycleOpenSidebar: () => void;
+  openSidebar: boolean;
 }
-function SideBar({ blogData, isSidebarOpen, toggleSidebar }: SidebarProps) {
-  const variants = useBreakpointValue({ base: smVariant, md: mdVariant })
-
+function SideBar({ blogData, isSidebarOpen, toggleSidebar,cycleOpenSidebar,openSidebar }: SidebarProps) {
+  const variants = useBreakpointValue(
+    { base: smVariant, md: mdVariant }
+  );
+  
   return (
     <SidebarDrawar
+      cycleOpenSidebar={cycleOpenSidebar}
+      openSidebar={openSidebar}
       blogData={blogData}
       variant={variants!.navigation}
       isOpen={isSidebarOpen}
       onClose={toggleSidebar}
     />
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
