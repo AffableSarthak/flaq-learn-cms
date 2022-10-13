235eb38a361cb47b07a9475a8fa6e32a2050c916
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-10-13 16:10:06 +0530

    loading animation in between the pages added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import 'react-notion-x/src/styles.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { ChakraProvider } from '@chakra-ui/react'
+import Router from "next/router";
+import NProgress from "nprogress"; 
+import "nprogress/nprogress.css"; 
+
+//Binding event for loading bar animation
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());  
 
 function MyApp({ Component, pageProps }: AppProps) {
   const getLayout = (page: any) => (
