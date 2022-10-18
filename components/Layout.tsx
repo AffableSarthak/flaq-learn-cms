e185ff008449e89c63d1ad3fa386f2311839669d
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-10-18 14:40:06 +0530

    font changed to nunito, bg and heading color change, margin on sm and md reduced

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,7 @@ import {
   Flex,
   useColorModeValue,
 } from "@chakra-ui/react";
+import Head from "next/head";
 import React from "react";
 import { BlogPages } from "../src/utils/parse-properties";
 import SideBar from "./Sidebar";
@@ -13,26 +14,37 @@ function Layout({
   children: React.ReactNode;
   blogData: BlogPages[];
 }) {
-  const bgColor = useColorModeValue("#2F3437", "#2F3437");
-  const textColor = useColorModeValue("#FBFBFA", "#2F3437");
+  const bgColor = useColorModeValue("#D0D1D2", "#D0D1D2");
+  const textColor = useColorModeValue("#FBFBFA", "#D0D1D2");
   return (
-    <Box position={"relative"} bg={bgColor} color={textColor}>
-      <Flex flexDirection={{ base: "column", md: "row", lg: "row" }}>
-        <Box
-          fontFamily={"Poppins"}
-          position={"relative"}
-          top={0}
-          left={0}
-          zIndex={1}
-        >
-          <SideBar blogData={blogData} />
-        </Box>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link href="xyz.com"></link>
+      </Head>
+      <Box position={"relative"} bg={bgColor} color={textColor}>
+        <Flex flexDirection={{ base: "column", md: "row", lg: "row" }}>
+          <Box
+            fontFamily={"Nunito Sans"}
+            position={"relative"}
+            top={0}
+            left={0}
+            zIndex={1}
+          >
+            <SideBar blogData={blogData} />
+          </Box>
 
-        <Box h="100vh" overflowY="scroll" flex="1">
-          {children}
-        </Box>
-      </Flex>
-    </Box>
+          <Box h="100vh" overflowY="scroll" flex="1">
+            {children}
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,20 +2,13 @@ import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 import theme from "../theme";
-
 type Props = {};
 
 class Document extends NextDocument<Props> {
   render() {
     return (
       <Html>
-        <Head>
-          {/* font Poppins from google font */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
