74f26fa0d75f86b983cc2f233b58def3550680bf
sarthak <sarthak>
2022-10-28 14:27:02 +0530

    Fix build errors

--- lib/ga/index.ts
+++ lib/ga/index.ts
@@ -1,9 +1,4 @@
 export const GA_TRACKING_ID = `${process.env.NEXT_PUBLIC_GA_ID}`;
-declare global {
-  interface Window {
-    gtag: any;
-  }
-}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
