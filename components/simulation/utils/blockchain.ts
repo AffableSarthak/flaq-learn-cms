1439890a8991e5490e0d7b82bcd4c0ed46078ef8
sarthak <sarthak>
2022-11-19 11:09:05 +0530

    Remove unused references

--- components/simulation/utils/blockchain.ts
+++ components/simulation/utils/blockchain.ts
@@ -3,34 +3,6 @@ import WalletIcon from "../../../public/img/simulations/wallets.svg";
 import algorandIcon from "../../../public/img/simulations/algorand.svg";
 import ethereumIcon from "../../../public/img/simulations/ethereum.svg";
 export const getBlockchainData = () => {
-  const data1 = [
-    {
-      name: "Solana",
-      icon: solanaIcon,
-      simulations: [
-        {
-          name: "Create Wallet",
-          url: "solana/create-wallet",
-          icon: WalletIcon,
-        },
-      ],
-    },
-    {
-      name: "Algorand",
-      icon: algorandIcon,
-      simulations: [
-        {
-          name: "Create Wallet",
-          url: "algorand/create-wallet",
-          icon: WalletIcon,
-        },
-      ],
-    },
-    {
-      name: "EVM",
-      simulations: [],
-    },
-  ];
   const data = [
     {
       name: "Solana",
@@ -40,7 +12,7 @@ export const getBlockchainData = () => {
           name: "Create Wallet",
           link: "solana/create-wallet",
           icon: WalletIcon,
-        }
+        },
       ],
     },
 
