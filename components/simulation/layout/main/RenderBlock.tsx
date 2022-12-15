4cd07f4f51a8fc09ada00627938827628007f928
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-12-15 23:37:26 +0530

    simulation redesign and header made sticky

--- components/simulation/layout/main/RenderBlock.tsx
+++ components/simulation/layout/main/RenderBlock.tsx
@@ -1,21 +1,21 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react'
-import React from 'react'
-import { BlockType } from '../../types'
-import links from '../../utils/links'
-import { RenderAccordianBlock } from './RenderAccordianBlock'
-import { RenderListBlock } from './RenderListBlock'
-import RenderParaBlock from './RenderParaBlock'
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import React from "react";
+import { BlockType } from "../../types";
+import links from "../../utils/links";
+import { RenderAccordianBlock } from "./RenderAccordianBlock";
+import { RenderListBlock } from "./RenderListBlock";
+import RenderParaBlock from "./RenderParaBlock";
 
 export const formatMentionText = (text: string, values: string[]) => {
-  const regex = new RegExp(/\[\[(.*?)\]\]/)
-  if (!values.length) return text
-  const split = text.split(regex)
+  const regex = new RegExp(/\[\[(.*?)\]\]/);
+  if (!values.length) return text;
+  const split = text.split(regex);
   return (
     <div>
       {
         // @ts-ignore
         split.reduce((prev: string, current: string, i: number) => {
-          if (!i) return [current]
+          if (!i) return [current];
 
           return prev.concat(
             // @ts-ignore
@@ -25,30 +25,37 @@ export const formatMentionText = (text: string, values: string[]) => {
               </Link>
             ) : (
               current
-            ),
-          )
+            )
+          );
         }, [])
       }
     </div>
-  )
-}
+  );
+};
 
 export const renderBlockTitle = (title: string) => {
   return (
     <Box>
-      <Text fontSize={'3xl'} fontWeight={'black'} textColor={'#a6ebc9'}>
+      <Text
+        color="#FFFFFF"
+        fontSize={{
+          base: "1rem",
+          md: "2rem",
+        }}
+        fontWeight={"600"}
+      >
         {title}
       </Text>
     </Box>
-  )
-}
+  );
+};
 
 function RenderBlock({ block }: { block: BlockType[] }) {
   const renderBlocks = (block: BlockType[]) => {
     return block.map((b, index) => {
-      const { accordianBlock, blockTitle, listBlock, paraBlock } = b
+      const { accordianBlock, blockTitle, listBlock, paraBlock } = b;
       return (
-        <Flex flexDirection={'column'} gap={2} key={index}>
+        <Flex flexDirection={"column"} gap={2} key={index}>
           <Box>{blockTitle ? renderBlockTitle(blockTitle) : <></>}</Box>
           <Box>{paraBlock ? <RenderParaBlock block={paraBlock} /> : <></>}</Box>
           <Box>{listBlock ? <RenderListBlock block={listBlock} /> : <></>}</Box>
@@ -60,11 +67,11 @@ function RenderBlock({ block }: { block: BlockType[] }) {
             )}
           </Box>
         </Flex>
-      )
-    })
-  }
+      );
+    });
+  };
 
-  return <Box my={4}>{renderBlocks(block)}</Box>
+  return <Box my={4}>{renderBlocks(block)}</Box>;
 }
 
-export default RenderBlock
+export default RenderBlock;
