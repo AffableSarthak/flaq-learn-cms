14f690c3de54d1ba27fb535a576ec982cd2f6f14
Harsh <harshgajjar145@gmail.com>
2023-01-27 17:33:04 +0530

    fixes flaq logo

--- pages/claim-nft.tsx
+++ pages/claim-nft.tsx
@@ -15,7 +15,7 @@ export default function claimNft() {
       <Flex justifyContent={"center"} position="relative">
         <Box
           w={"540px"}
-          h={["unset", "626px"]}
+          h={["unset", "600px"]}
           mb={[2, 72]}
           pt={["250px", "105px"]}
           textAlign="center"
@@ -23,6 +23,17 @@ export default function claimNft() {
           backgroundPosition="center"
           backgroundSize={"contain"}
           backgroundRepeat={"no-repeat"}
+          position="relative"
+          _before={{
+            content: `""`,
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            backgroundColor: "rgba(0,0,0,0.1)",
+            backdropBlur: "10px",
+          }}
         >
           <Heading fontFamily={"Poppins"} fontSize={["32px", "40px", "60px"]}>
             Congratulations!
@@ -97,7 +108,7 @@ export default function claimNft() {
           src="/img/claimNft/Blob1.png"
           position={"absolute"}
           top={"400px"}
-          right={"190px"}
+          right={[0, "10px", "190px"]}
           w="290px"
           display={["none", "none", "block"]}
         />
